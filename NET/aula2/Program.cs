c32cfa622e075705064c74c1a424cd6e2302bcdd
otaviotursi <62893377+otaviotursi@users.noreply.github.com>
2021-07-27 11:27:41 -0300

    Initial commit

---
+++ NET/aula2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
